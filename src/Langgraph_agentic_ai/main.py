1e1b86460768fd4b011318e4833d896715dc1db8
Manas Dhurve <yogendhurve2002@gmail.com>
2026-09-08 20:08:31 +0530

    feat: Integrate Streamlit UI with application
    - Connected the Streamlit UI with the application flow
    - Added UI initialization and user input handling
    - Added application entry point for launching the app
    - Added initial application-level error handling

---
+++ src/Langgraph_agentic_ai/main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from src.Langgraph_agentic_ai.ui.Streamlit_ui.load_ui import LoadStreamlitUI
+
+
+def load_langgraph_agenticai_app():
+    """
+    Loads and runs the LangGraph AgenticAI application with Streamlit UI.
+    This function initializes the UI, handles user input, configures the LLM model,
+    sets up the graph based on the selected use case, and displays the output while 
+    implementing exception handling for robustness.
+
+    """
+
+    ##Load UI
+    ui=LoadStreamlitUI()
+    user_input=ui.load_streamlit_ui()
+
+    if not user_input:
+        st.error("Error: Failed to load user input from the UI.")
+        return
+    
+    user_message = st.chat_input("Enter your message:")
