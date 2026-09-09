03d1f05b5714826e2e1349832c28f117a8820c03
Yogen Dhurve <yogendhurve2002@gmail.com>
2026-09-09 22:38:46 +0530

    feat: build basic chatbot graph
    - Added LangGraph workflow for the basic chatbot use case
    - Connected chatbot node between START and END
    - Added use-case based graph configuration
    - Added graph compilation for execution

--- src/Langgraph_agentic_ai/Graph/graph_builder.py
+++ src/Langgraph_agentic_ai/Graph/graph_builder.py
@@ -22,3 +22,12 @@ class GraphBuilder:
         self.graph_builder.add_node("chatbot",self.basic_chatbot_node.process)
         self.graph_builder.add_edge(START,"chatbot")
         self.graph_builder.add_edge("chatbot",END)
+
+    def setup_graph(self, usecase: str):
+        """
+        Sets up the graph for the selected use case.
+        """
+        if usecase == "Basic Chatbot":
+            self.basic_chatbot_build_graph()
+
+        return self.graph_builder.compile()
