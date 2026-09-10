2e82a005428687dc886e3636c0c27f5edbdd640b
Yogen Dhurve <yogendhurve2002@gmail.com>
2026-09-10 20:43:53 +0530

    feat: integrate Streamlit UI with application
    - Connected Streamlit UI with the application flow
    - Added LLM configuration and model initialization
    - Added use-case based graph setup
    - Integrated graph execution with result display
    - Added application-level error handling
    - Updated available Groq models in UI configuration

--- src/Langgraph_agentic_ai/main.py
+++ src/Langgraph_agentic_ai/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from src.Langgraph_agentic_ai.ui.Streamlit_ui.load_ui import LoadStreamlitUI
-
+from src.Langgraph_agentic_ai.LLMs.groqllm import GroqLLM
+from src.Langgraph_agentic_ai.Graph.graph_builder import GraphBuilder
+from src.Langgraph_agentic_ai.ui.Streamlit_ui.display_result import DisplayResultStreamlit
 
 def load_langgraph_agenticai_app():
     """
@@ -20,3 +22,35 @@ def load_langgraph_agenticai_app():
         return
     
     user_message = st.chat_input("Enter your message:")
+
+    if user_message:
+        try:
+            ## Configure The LLM's
+            obj_llm_config=GroqLLM(user_contols_input=user_input)
+            model=obj_llm_config.get_llm_model()
+
+            if not model:
+                st.error("Error: LLM model could not be initialized")
+                return
+            
+            # Initialize and set up the graph based on use case
+            usecase=user_input.get("selected_usecase")
+
+            if not usecase:
+                    st.error("Error: No use case selected.")
+                    return
+            
+            ## Graph Builder
+
+            graph_builder=GraphBuilder(model)
+            try:
+                 graph=graph_builder.setup_graph(usecase)
+                 print(user_message)
+                 DisplayResultStreamlit(usecase,graph,user_message).display_result_on_ui()
+            except Exception as e:
+                 st.error(f"Error: Graph set up failed- {e}")
+                 return
+
+        except Exception as e:
+             st.error(f"Error: Graph set up failed- {e}")
+             return   
